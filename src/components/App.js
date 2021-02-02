37ee4f18992196a6e89527ec0b0b254417e5bc3c
robdcampbell <rdeancampbell@gmail.com>
2021-02-02 07:46:32 -0500

    Signup.js Component created after installing bootstrap and react-bootstrap. Moving onto User-Context creation

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
+const Signup = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" required ref={emailRef} />
+            </Form.Group>
+
+            <Form.Group id="password">
+              <Form.Label>Passwrod</Form.Label>
+              <Form.Control type="password" required ref={passwordRef} />
+            </Form.Group>
+
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" required ref={passwordConfirmRef} />
+            </Form.Group>
+
+            <Button type="submit" className="w-100">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log In
+      </div>
+    </>
+  );
+};
+
+export default Signup;
