f033bd4bd80146239769a1bebc16e8018da1774c
robdcampbell <rdeancampbell@gmail.com>
2021-02-03 07:07:44 -0500

    React router added, login and dashboard components created

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,27 @@ import React from "react";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <Container
-          className="d-flex align-items-center justify-content-center"
-          style={{ minHeight: "100vh" }}
-        >
-          <div className="w-100" style={{ maxWidth: "400px" }}>
-            <Signup />
-          </div>
-        </Container>
-      </AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  //get signup function from created AuthContext
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      // signup function
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch (e) {
+      setError("Failed to create an account...");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {/* firebase sets localStorage tokens to verify 
+          {currentUser.email} */}
+          {error && <Alert variant="danger">{error}</Alert>}
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" required ref={emailRef} />
+            </Form.Group>
+
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" required ref={passwordRef} />
+            </Form.Group>
+
+            <Button disabled={loading} type="submit" className="w-100">
+              Log In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  );
+};
+
+export default Login;
